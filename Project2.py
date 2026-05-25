5209310ee6ae1480503dfce8356bd226612c8aa9
ZAMIN RAZA <zaminraza329z@gmail.com>
2026-05-25 16:09:25 +0500

    Add files via upload

---
+++ Project2.py
@@ -0,0 +1,9 @@
+total = 0
+print("=== Expense Tracker ===")
+while True:
+    expense = input("Enter expense amount (or type 'done' to finish): ")
+
+    if expense.lower() == "done":
+        break
+    total = total + float(expense)
+print("\nTotal Spent =", total)
